b8f14cd7e846cac483679aee74881ac95c3460b8
hocine <hocine737718@gmail.com>
2024-02-25 23:53:19 +0100

    Home About Commit

---
+++ src/components/Home/AboutCmp.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="home_about" id="home_about">
+        <span class="about_h_back">
+        <div class="about_h_title">
+            Qui sommes-nous
+        </div>
+        <div class="about_h_content">
+            <p>
+                Le cabinet El Djazairia Likhadamat el Idaria Moutaadida, Sarl (par abréviation EDLIM) a été créé en Avril 2010 ; nous comptons plus de 20 salariés et de nombreux consultants externes spécialisés dans l'administration des ressources humaines avec tous ses aspects opérationnels ainsi que le conseil en droit social (droit du travail, sécurité sociale et fiscalité salariale).
+                EDLIM possède les compétences, les ressources et le savoir-faire nécessaires pour relever vos défis en matière d’administration des ressources humaines.
+            </p>
+            <a href="/qui-somme-nous">Voir plus</a>            
+        </div>
+
+        </span>
+    </div>
+</template>
+
+<script>
+import ScrollReveal from 'scrollreveal';
+export default {
+    name:'AboutCmp',
+    mounted(){
+        const sr = ScrollReveal({
+            origin: 'top',
+            distance: '60px',
+            duration: 2500,
+            delay: 400
+        })
+        sr.reveal(`.about_h_content`);
+    }
+}
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,20 +1,21 @@
 <template>
   <div class="home">
     <Slider/>
+    <About/>
   </div>
 </template>
 
 <script>
 import Slider from '@/components/Home/SliderCmp.vue'
 //import Services from '@/components/Home/Services/ServicesCmp.vue'
-//import About from '@/components/Home/AboutCmp.vue'
+import About from '@/components/Home/AboutCmp.vue'
 import '@/assets/css/Home/home.css'
 import '@/assets/css/Home/media_home.css'
 export default {
   name: 'HomeView',
   components: {
     Slider,
-    //About,
+    About,
     //Services
   }
 }
